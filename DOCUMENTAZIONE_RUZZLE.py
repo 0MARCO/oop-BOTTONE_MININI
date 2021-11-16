d90a2489aa090f409fc6dab316d6d2175ad9a6bf
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-11-16 20:23:55 +0100

    Update DOCUMENTAZIONE_RUZZLE.py

--- DOCUMENTAZIONE_RUZZLE.py
+++ DOCUMENTAZIONE_RUZZLE.py
@@ -1,11 +1,16 @@
                                                                                    RUZZLE
 Legenda
-  1 In che cosa consiste Ruzzle
-  2 Regole del gioco
+  1 Che cos' è Ruzzle
+  2 Come è stato progettato 
+  3 Regole del gioco
 
 
   -PRIMO PARAGRAFO-
-    Questo gioco consiste....
+    Ruzzle e’ un gioco interattivo online in cui l’obiettivo e’ quello di trovare il maggior numero di parole all'interno di una griglia in un tempo prestabilito 
+    appariranno delle lettere all’interno della griglia che se utilizzate all’interno di una parola,
+    andranno ad aumentare il punteggio che messo a confronto con quello dell’avversario(online) andra’ a determinare la vittoria o la sconfitta del  singolo round, 
+    i round sono 3 e chi avra’ collezionato piu’ punti si aggiudichera’ la vittoria.
+
 
   -SECONDO PARAGRAFO-
     Le regole di questo gioco...
