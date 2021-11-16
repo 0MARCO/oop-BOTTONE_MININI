913c1c864ad7b7d486644bae12383f3789286718
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-11-16 20:08:09 +0100

    Update DOCUMENTAZIONE_RUZZLE.py

--- DOCUMENTAZIONE_RUZZLE.py
+++ DOCUMENTAZIONE_RUZZLE.py
@@ -1,2 +1,11 @@
-RUZZLE
+                                                                                   RUZZLE
+Legenda
+  1 In che cosa consiste Ruzzle
+  2 Regole del gioco
 
+
+  -PRIMO PARAGRAFO-
+    Questo gioco consiste....
+
+  -SECONDO PARAGRAFO-
+    Le regole di questo gioco...
