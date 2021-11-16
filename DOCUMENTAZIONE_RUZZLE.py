df5951a3dcd2b774c8dc359403d43726bd97cead
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-11-16 20:34:14 +0100

    Update DOCUMENTAZIONE_RUZZLE.py

--- DOCUMENTAZIONE_RUZZLE.py
+++ DOCUMENTAZIONE_RUZZLE.py
@@ -13,4 +13,13 @@ Legenda
 
 
   -SECONDO PARAGRAFO-
-    Le regole di questo gioco...
+    Quest’anno sempre programmando in python abbiamo deciso di utilizzare la compresenza in matematica per creare un gioco interattivo,
+    abbiamo prima seguito un percorso in matematica con la professoressa tripepi in cui abbiamo studiato il calcolo combinatorio, ovvero, 
+    in quanti modi e’ possibile disporre una certa quantita’ di elementi su un determinato quantitativo di posti.
+    Abbiamo poi pensato che ruzzle fosse un ottimo gioco da creare per mettere in pratica lo studio che avevamo effettuato e quindi,
+    il nostro obiettivo quest’ anno e’ quello di collaborare per arrivare al risultato finale:
+    ognuno provera’ a scrivere delle righe di codice e si prenderanno i migliori codici relativi ad ogni funzione necessaria,
+    mettendo tutto insieme, riusciremo poi a creare il nostro gioco interattivo.
+    Con l’aiuto di github riusciamo a comunicare molto facilmente e a provare a scrivere con l’aiuto del professore il codice in questione.
+    Il professore mazzone ci sta aiutando scrivendo le funzioni (da modificare naturalmente)  in poche righe di codice in modo da avere tutto piu’ chiaro.
+
