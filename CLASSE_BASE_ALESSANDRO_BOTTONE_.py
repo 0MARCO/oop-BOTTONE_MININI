31f76fe04d9ab0b8b714fb96436563c021497047
0MARCO <75375475+0MARCO@users.noreply.github.com>
2021-11-17 16:55:33 +0100

    Update CLASSE_BASE_ALESSANDRO_BOTTONE_.py

--- CLASSE_BASE_ALESSANDRO_BOTTONE_.py
+++ CLASSE_BASE_ALESSANDRO_BOTTONE_.py
@@ -37,7 +37,7 @@ class calcComb():
         line = f.readline()
 
         for line in f:  # per ogni riga del file vengono eseguite le righe di codice che seguono
-        #    print(str) 
+            print(str) 
             if str == line[:-1]:  #bisogna eliminare l'ultimo carattere dalla parola contenuta nella riga del file
                 print("vero")
         '''
